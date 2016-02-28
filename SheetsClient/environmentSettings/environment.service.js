c37c0a5879d85cb86a7a9327d663a8f272bbd24d
Enrico Piccinin <enrico.piccinin@MacBook-Pro-di-Enrico.local>
2016-02-29 00:00:44 +0100

    mail support and config for AWS

--- SheetsClient/environmentSettings/environment.service.js
+++ SheetsClient/environmentSettings/environment.service.js
@@ -5,8 +5,8 @@ System.register([], function(exports_1) {
         execute: function() {
             Environment = (function () {
                 function Environment() {
-                    this.baseServiceUrl = 'http://localhost:3005/sheets/';
-                    //public baseServiceUrl = 'http://ec2-54-213-172-98.us-west-2.compute.amazonaws.com:3005/sheets/';
+                    //public baseServiceUrl = 'http://localhost:3005/sheets/';
+                    this.baseServiceUrl = 'http://ec2-54-213-172-98.us-west-2.compute.amazonaws.com:3005/sheets/';
                     this.stockPricesServiceUrlStart = "https://www.quandl.com/api/v3/datasets/WIKI/";
                     this.stockPricesServiceUrlEnd = ".csv?api_key=oVMreULVs1AP7stTsyA4&limit=1";
                 }
