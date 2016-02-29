ee703eb35de1d7009358eb1361478be2910d9a9b
Enrico Piccinin <enrico.piccinin@MacBook-Pro-di-Enrico.local>
2016-02-29 10:55:34 +0100

    apache config file added 1

--- SheetsClient/environmentSettings/environment.service.ts
+++ SheetsClient/environmentSettings/environment.service.ts
@@ -1,6 +1,6 @@
 export class Environment { 
-    public baseServiceUrl = 'http://localhost:3005/sheets/';
-    //public baseServiceUrl = 'http://ec2-54-213-172-98.us-west-2.compute.amazonaws.com:3005/sheets/';
+    //public baseServiceUrl = 'http://localhost:3005/sheets/';
+    public baseServiceUrl = 'http://ec2-54-213-172-98.us-west-2.compute.amazonaws.com:3005/sheets/';
     public stockPricesServiceUrlStart = "https://www.quandl.com/api/v3/datasets/WIKI/";
     public stockPricesServiceUrlEnd = ".csv?api_key=oVMreULVs1AP7stTsyA4&limit=1";
 }
